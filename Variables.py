97f415cd5fd032c852019abfc78cfa5dee5632ae
Mendelsom <yuri.babkin@gmail.com>
2024-10-14 13:16:30 +0300

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,5 @@
+number_of_hw = 12
+hours = 1.5
+course_name = 'Python'
+time_per_hw = hours / number_of_hw
+print(f'Курс: {course_name}, всего задач: {number_of_hw}, затрачено часов: {hours}, среднее время выполнения: {time_per_hw}.')
