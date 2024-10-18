e2f4a0fccd1a911b1f1738ac6c9a72736102c30c
Mendelsom <yuri.babkin@gmail.com>
2024-10-18 13:46:09 +0300

    Add files via upload

---
+++ Dop_Zad_Dannye.py
@@ -0,0 +1,22 @@
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+
+#Convert set to list and sort
+list_o_st = []
+for i in range(len(students)):
+    st_1 = [students.pop()]
+    list_o_st = list_o_st + st_1
+list_sort = sorted(list_o_st)
+
+#calculate average grades ans create dictionary
+aver = []
+dict_ = dict()
+for i in range(0,(len(grades))):
+    sum_ = 0
+    for n in range (0,(len(grades[i]))):
+        print(n)
+        sum_ = sum_ + grades[i][n]
+    print(sum_)
+    aver.append(sum_ / (len(grades[i])))
+    dict_[list_sort[i]] = aver[i]
+print(dict_)
