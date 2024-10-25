485e44146190c766a8ccffa62da68cec2cf923fd
Mendelsom <yuri.babkin@gmail.com>
2024-10-25 19:01:11 +0300

    Add files via upload

---
+++ module_2_hard.py
@@ -0,0 +1,27 @@
+number = int(input('number '))
+posled = [] #list returning encripted number
+j = 0 # j is a counter of cases when the  number is dividable for smaller number
+for i in range(1, number):
+    x_ = number - i
+    # print(x_)
+    y_ = number - x_
+    # print(y_)
+
+    if y_ >= x_: # check if a number is
+        break
+    elif number % (i + 2) == 0: # check if a number is dividable for smaller numbers
+        j += 1
+        x_1 = (i + 2) -1
+        y_1 = (i + 2) - x_1
+        posled.insert(j - 1, x_1)
+        posled.insert(j - 1, y_1)
+        posled.append(y_)
+        posled.append(x_)
+    else:
+        posled.append(y_)
+        posled.append(x_)
+print(''.join(map(str, posled))) #converting list to string
+
+
+
+
