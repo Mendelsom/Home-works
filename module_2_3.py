2913b21b36e645ef8737d3afe38f3cc47a1b6fb9
Mendelsom <yuri.babkin@gmail.com>
2024-11-01 23:21:35 +0300

    Update module_2_3.py

--- module_2_3.py
+++ module_2_3.py
@@ -1,9 +1,13 @@
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 print(my_list)
 i = 0
+# my_list_1 = []
 while i <= len(my_list):
+
     if my_list[i] > 0:
         print(my_list[i])
-        i = i + 1
+        i += 1
+    elif my_list[i] == 0:
+        i += 1
     else:
         break
