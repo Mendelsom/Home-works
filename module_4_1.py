32e2e54ddc0da9f044fabb48a3b8ded47cbe265b
Mendelsom <yuri.babkin@gmail.com>
2024-11-12 15:21:36 +0300

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,10 @@
+import fake_muth as fk
+import true_math as tr
+result1 = fk.divide(69, 3)
+result2 = fk.divide(3, 0)
+result3 = tr.divide(49, 7)
+result4 = tr.divide(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
