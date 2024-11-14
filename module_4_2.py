9c44ec64456301a4ff9a9e82c24966a127f0a043
Mendelsom <yuri.babkin@gmail.com>
2024-11-14 12:19:25 +0300

    Create module_4_2.py

---
+++ module_4_2.py
@@ -0,0 +1,7 @@
+def test_function():
+    def inner_function():
+        print('Я в области видимости test_function')
+    inner_function()
+
+# вызов inner_function() вне test_function - ошибка, имя не определено.
+inner_function()
