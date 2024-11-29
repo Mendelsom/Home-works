ec00d36a70bf7b516daba95446fd0a082ba8e2eb
Mendelsom <yuri.babkin@gmail.com>
2024-11-29 23:37:02 +0300

    Update module_2_hard.py

--- module_2_hard.py
+++ module_2_hard.py
@@ -1,23 +1,23 @@
 number = int(input('number '))
-posled = [] #list returning encripted number
-j = 0 # j is a counter of cases when the  number is dividable for smaller number
+posled = []  # list returning encripted number
+j = 0  # j is a counter of cases when the  number is dividable for smaller number
 for i in range(1, number):
     x_ = number - i
     y_ = number - x_
-    if y_ >= x_: # check if a number is
+    if y_ >= x_:  # check if a number is
         break
-    elif number % (i + 2) == 0: # check if a number is dividable for smaller numbers
-        j += 2
-        x_1 = (i + 2) -1
-        y_1 = (i + 2) - x_1
-        posled.insert(j - 2, x_1)
-        posled.insert(j - 2, y_1)
-        posled.append(y_)
-        posled.append(x_)
+        if number % (i + 2) == 0:  # check if a number is dividable for smaller numbers
+            j += 2
+            x_1 = (i + 2) - 1
+            y_1 = (i + 2) - x_1
+            posled.insert(j - 2, x_1)
+            posled.insert(j - 2, y_1)
+            posled.append(y_)
+            posled.append(x_)
     else:
         posled.append(y_)
         posled.append(x_)
-print(''.join(map(str, posled))) #converting list to string
+print(''.join(map(str, posled)))  # converting list to string
 
 
 
